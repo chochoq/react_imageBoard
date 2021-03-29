bc38064f14c383ed522c698e4372d3e7702fac9a
chochoq <aubepe@gmail.com>
2021-03-29 17:07:04 +0900

    (add) PostList.js

---
+++ src/components/Post.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Post = () => {
+    
+    return (
+        <React.Fragment>
+            <div>유저프로필/ 유저이름/ 게시글 작성시간/ 나인지확인하는버튼(수정)</div>
+            <div>게시물내용</div>
+            <div>사진</div>
+            <div>댓글갯수</div>
+        </React.Fragment>
+    )
+}
+
+// props가 없어서 오류가 날것을 방지해 미리 defaultProps를 넣어준다
+Post.defaultProps = {
+    user_info: {
+        user_name: "보로꼬리",
+        user_profile: "https://foodsweeat.co.kr/wp-content/uploads/broccoli-1238250_1280-1.jpg"
+    },
+    image_url:"https://foodsweeat.co.kr/wp-content/uploads/broccoli-1238250_1280-1.jpg",
+    contents: "안녕하세요 보로꼬리입니다 만관잘부",
+    comment_cnt: 10,
+    insert_dt:"2021-02-02 11:00:00",
+    
+};
+
+export default Post;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostList from '../pages/PostList';
+
 function App() {
   return (
     <React.Fragment>
-      안뇽 리액트
+      <BrowserRouter>
+        <Route path="/" exact component={PostList} />
+      </BrowserRouter>
     </React.Fragment>
   );
 }
